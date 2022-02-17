40152f0fb311de9a53e6f5e6bc35877e8d0530ce
kikonavarro <78880702+kikonavarro@users.noreply.github.com>
2022-02-17 22:23:03 +0100

    comenzando mongoose conexion

---
+++ models/connectMongoose.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+mongoose.connection.on('error', (err) => {
+	console.log('Error de conexión', err);
+	process.exit(1);
+});
+
+mongoose.connection.once('open', () => {
+	//console.log('Conectado a MongoDB en', mongoose.connection.name);
+	console.log('Conectado a MongoDB');
+});
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STR, {
+	
+});
+
+module.exports = mongoose.connection;
