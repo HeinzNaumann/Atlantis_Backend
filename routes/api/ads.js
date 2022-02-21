3f9ca4b68b1d3d82b0a299283747b7c1ac4e7387
Diego Perez <route1024@gmail.com>
2022-02-22 00:01:39 +0100

    Metodo list, show, delete, update Anuncios, rev 1

--- routes/api/ads.js
+++ routes/api/ads.js
@@ -1,24 +1,43 @@
 const express = require("express");
+const router = express.Router();
 const Anuncio = require("../../models/Anuncio.js");
 //const jwtAuth = require("../../lib/jwtAuthMiddleware");
+const multer  = require('multer');
+const path = require('path');
+
+//TODO
+//Borrar imagenes con el delete, posibles problema con permisos en servidor
+//actualizar con PUT
+//capturar ID de usuario al crear anuncio generado en jwtAuthMiddleware antes de las peticiones (req.apiAuthUserId)
 
 
-
-const router = express.Router();
+//Multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../../public/images/anuncios'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
 
 // /api/ads
 //Lista de anuncios
-router.get("/ads", /*jwtAuth*/ async (req, res, next) =>{
+router.get("/", /*jwtAuth*/ async (req, res, next) =>{
     try{
-        console.log("entrando en api")
+        //console.log("entrando en api")
         const name = req.query.name;
         const price = req.query.price;
         const tag = req.query.tag;
         const skip = parseInt(req.query.skip);
-        const limit = parseInt(req.query.limit);
+        const limit = parseInt(req.query.limit) || 1000; // nuestro api devuelve max 1000 registros;
         const select = req.query.select; // campos
         const sort = req.query.sort;
+        //const start = parseInt(req.query.start) || 0;
+
         
 
         const filter = {};
@@ -33,7 +52,7 @@ router.get("/ads", /*jwtAuth*/ async (req, res, next) =>{
         }
 
         const anuncios = await Anuncio.lista(filter, skip, limit, select, sort);
-        console.log("AQUI", anuncios)
+        //console.log("AQUI", anuncios)
         res.json({ results: anuncios });
     }catch (err){
         next(err)
@@ -56,15 +75,16 @@ router.get("/:identificador", async (req, res, next) =>{
 //const path = require("path");
 //POST /api/ads (body)
 //Crear un anuncio
-router.post("/", async (req, res, next) =>{
+router.post("/", upload.single('imagen'),async (req, res, next) =>{
     
+    console.log("Entra en API POST");
     try{
-
+        const filename =req.file.filename;
         const anuncioData = {
           ...req.body,
           //image: req.file.path,
-        };
-        
+         imagen: filename
+        };        
 
         const anuncio = new Anuncio(anuncioData);
 
@@ -96,9 +116,13 @@ router.delete("/:id", async (req, res, next) =>{
 //Actualizar un anuncio
 router.put("/:id", async (req, res, next) =>{
     try{
-
+        
+        //console.log("Entra en PUT");
         const _id = req.params.id;
         const anuncioData = req.body;
+
+        //
+        console.log("Body Req:", req.body);
 
         const updatedAnuncio = await Anuncio.findOneAndUpdate({ _id: _id}, anuncioData, {
             new: true
