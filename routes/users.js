5ff3b0d8a5a7745103b8c8af75599411e3d73572
kikonavarro <78880702+kikonavarro@users.noreply.github.com>
2022-02-23 18:23:27 +0100

    Creado Delete user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+const Usuario = require('../models')
+const jwtAuthMiddleware = require('../lib/jwtAuthMiddleware')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+/**
+* DELETE user
+* Habrá que borrar todos sus anuncios creados
+*/
+
+
+router.delete('/:id', jwtAuthMiddleware, async (req, res, next)=> {
+
+  try {
+    console.log(`El usuario ${_id} ha sido eliminado`);
+    await User.deleteOne({ _id: _id });
+    res.json();
+  } catch (error) {
+    next(error);
+  };
+})
 
 module.exports = router;
